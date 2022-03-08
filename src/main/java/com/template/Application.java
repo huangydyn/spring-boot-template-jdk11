7c914ed11c2f73e20d59acb481277518f7468ea1
huangyan <yxhuang@thoughtworks.com>
2022-03-08 22:00:22 +0800

    delete db and redis

--- src/main/java/com/template/Application.java
+++ src/main/java/com/template/Application.java
@@ -2,11 +2,9 @@ package com.template;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableCaching
 @SpringCloudApplication
 @SuppressWarnings("PMD.UseUtilityClass")
 @EnableFeignClients(basePackages = "com.template")
